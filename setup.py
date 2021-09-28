3b715fba47147fcf654cfb5001432e5bf504e0ac
Reza <reza@mplus.software>
2021-09-28 10:54:28 +0700

    add cookiejar

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
 	include_package_data = True,
 
 	# Package dependencies.
-	install_requires = ['mako', 'suds-py3', 'cookiejar'],
+	install_requires = ['cookiejar', 'mako', 'suds-py3'],
 
 	# Metadata for PyPI.
 	author = 'David Béal',
