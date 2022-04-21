9500315525a27e6f500789db7715dbdfb0ca7245
Hadyan Adam <hadyanadamn@gmail.com>
2022-04-21 19:58:04 +0700

    import the data to init

--- laposte_api/__init__.py
+++ laposte_api/__init__.py
@@ -1,5 +1,5 @@
 from . import colissimo_and_so, countries, exception_helper, label_helper
-from .data import (colissimo_7Q_anht23, colissimo_7Q_anht33,
+from data import (colissimo_7Q_anht23, colissimo_7Q_anht33,
                     colissimo_7Q_camet11, colissimo_7Q_camet12,
                     colissimo_7Q_camet14, colissimo_8Q_ahj2,
                     colissimo_8Q_stou2, colissimo_9L_hamp13b,
