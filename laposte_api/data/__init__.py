86d1653c5def791c44e79fb4e840154928eb9b6e
Hadyan Adam <hadyanadamn@gmail.com>
2022-04-21 20:04:08 +0700

    import the data to init

--- laposte_api/data/__init__.py
+++ laposte_api/data/__init__.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 
-import colissimo_7Q_anht23  # noqa
-import colissimo_7Q_anht33  # noqa
-import colissimo_7Q_camet11  # noqa
-import colissimo_7Q_camet12  # noqa
-import colissimo_7Q_camet14  # noqa
-import colissimo_8Q_ahj2  # noqa
-import colissimo_8Q_stou2  # noqa
-import colissimo_9L_hamp13b  # noqa
-import colissimo_9L_hamp23b  # noqa
-import colissimo_9L_nhas52b  # noqa
-import colissimo_9V_hamp13  # noqa
-import colissimo_9V_nhas11  # noqa
-import colissimo_9V_nhas22  # noqa
+from . import colissimo_7Q_anht23  # noqa
+from . import colissimo_7Q_anht33  # noqa
+from . import colissimo_7Q_camet11  # noqa
+from . import colissimo_7Q_camet12  # noqa
+from . import colissimo_7Q_camet14  # noqa
+from . import colissimo_8Q_ahj2  # noqa
+from . import colissimo_8Q_stou2  # noqa
+from . import colissimo_9L_hamp13b  # noqa
+from . import colissimo_9L_hamp23b  # noqa
+from . import colissimo_9L_nhas52b  # noqa
+from . import colissimo_9V_hamp13  # noqa
+from . import colissimo_9V_nhas11  # noqa
+from . import colissimo_9V_nhas22  # noqa
